e7fcd0edc2716f7c99843d9d92a4271595444434
Miguel-CaAr <castroarredondo.hector@gmail.com>
2023-11-04 02:05:14 -0700

    Buscador en multiples contenedores

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import { api } from "./data/api.js";
-import { displayMovies } from './components/display.js'
+import { displayMovies } from "./components/display.js";
 import { search } from "./components/search.js";
 
 /**
  * Obtiene el JSON y renderiza las peliculas.
  */
-async function getMovies() {
-  const movies = await api();
-  displayMovies(movies.results);
-  search(movies.results);
+async function App() {
+  //PELICULAS POPULARES
+  const premiereMovies = await api("now_playing"); //Obtiene JSON segun el argumento (path segment)
+  displayMovies(premiereMovies.results, ".premiereMovies"); //Renderiza las peliculas en el contenedor HTML
+  //PELICULAS ESTRENO
+  const upcomingMovies = await api("upcoming");
+  displayMovies(upcomingMovies.results, ".upcomingMovies");
+
+  //BUSCADOR
+  const movies = premiereMovies.results.concat(upcomingMovies.results); //Concatenamos los arreglos de los objetos (.results es la propiedad array)
+  search(movies); //Buscador en el contenedor HTML
 }
 
-getMovies();
+App();
