09d0f8572929ec89ab09f0166344a35c21b6547f
Miguel-CaAr <castroarredondo.hector@gmail.com>
2023-11-04 02:04:58 -0700

    Segundo parametro para renderizar peliculas en mutiples contenedores

--- components/display.js
+++ components/display.js
@@ -2,21 +2,22 @@ import { posterMovie } from "./poster.js";
 
 /**
  * Recorre el JSON para generar elementos HTML por cada pelicula.
- * @param {object} movies Recibe el JSON
+ * @param {object} movies JSON
+ * @param {string} contenedor Clase del elemento html, para renderizar peliculas dentro
  */
-export function displayMovies(movies) {
-    movies.forEach((movie) => {
-      const container = document.querySelector(".listMovies");
-  
-      const card = document.createElement("li");
-      card.className = "card";
-      card.id = movie.title;
-  
-      const poster = document.createElement("img");
-      poster.className = "poster";
-      poster.src = `${posterMovie(movie.poster_path)}`;
-  
-      card.appendChild(poster);
-      container.appendChild(card);
-    });
-  }
+export function displayMovies(movies, contenedor) {
+  movies.forEach((movie) => {
+    const container = document.querySelector(contenedor);
+
+    const card = document.createElement("li");
+    card.className = "card";
+    card.id = movie.title;
+
+    const poster = document.createElement("img");
+    poster.className = "poster";
+    poster.src = `${posterMovie(movie.poster_path)}`;
+
+    card.appendChild(poster);
+    container.appendChild(card);
+  });
+}
