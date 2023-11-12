075c1a63ee485c732b62aed0db23aea8ac3e3ac6
Miguel-CaAr <castroarredondo.hector@gmail.com>
2023-11-12 12:25:15 -0800

    Se agrego efecto visual para cuado se agrege y para cuando ya esta en la lista de ver despues

--- components/Modals/optionsModalMovie.js
+++ components/Modals/optionsModalMovie.js
@@ -8,11 +8,20 @@ export function optionsModal(footerModal, movie) {
   //Evento para guarda la pelicula en la lista de ver despues
   btnSeeLater.addEventListener("click", () => {
     localStorage.setItem(movie.title, movie.poster_path);
+    //Se cambia el boton para indicar que fue añadida
+    btnSeeLater.innerHTML = "✅ Añadida";
+    btnSeeLater.className = "btnSeeLaterAdded";
   });
-  btnSeeLater.innerHTML = "📅 Ver despues";
-  btnSeeLater.className = "btnSeeLater";
-  footerModal.appendChild(btnSeeLater);
-
+  //Cada vez que se abre la movie se verifica si esta añadida
+  if (Object.keys(localStorage).includes(movie.title)) {
+    btnSeeLater.innerHTML = "✅ Añadida";
+    btnSeeLater.className = "btnSeeLaterAdded";
+    footerModal.appendChild(btnSeeLater);
+  } else {
+    btnSeeLater.innerHTML = "📅 Ver despues";
+    btnSeeLater.className = "btnSeeLater";
+    footerModal.appendChild(btnSeeLater);
+  }
   //Aun no se que hara quiza ver video en yt
   const aunNoSe = document.createElement("button");
   aunNoSe.className = "aunNoSe";
